f4e79ef3b6e0c0a6db3f68c039e47d6ffca1b6ea
Ricky Zheng <rickzheng19@gmail.com>
2023-06-20 16:15:28 -0400

    Initial Commit

---
+++ DessertRecipes/DessertRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertRecipesApp.swift
+//  DessertRecipes
+//
+//  Created by Ricky Zheng on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct DessertRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
